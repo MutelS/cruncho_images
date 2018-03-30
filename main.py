36e947f7a136a85f93a1634ff94be95a3820e1fd
sergei <mutel.sergey@gmail.com>
2018-03-30 15:44:39 +0300

    v3

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ import logging
 import urllib2
 import webapp2
 import settings
+
 from PIL import Image
 
-from google.appengine.api import images
-from google.appengine.api.images import images_service_pb
-from google.appengine.api import taskqueue
+from google.appengine.api import images, taskqueue
 from google.cloud.storage import Client
-from google.appengine.ext import db, blobstore
-#from google.appengine.api import users
+from google.appengine.ext import db
+# from google.appengine.api import users
 
-#from cors.cors_application import CorsApplication
-#from cors.cors_options import CorsOptions
+# from cors.cors_application import CorsApplication
+# from cors.cors_options import CorsOptions
 
 from requests_toolbelt.adapters import appengine
-
 appengine.monkeypatch()
 
 
@@ -23,109 +21,172 @@ def get_storage_client():
     return Client(project=settings.PROJECT_NAME)
 
 
+def resize_image(image, new_image):
+    image.resize(width=int(new_image.image_width), height=int(new_image.image_height))
+    return image
+
+
+def get_image_by_url(url):
+    image_at_url = urllib2.urlopen(url)
+    image_bytes = image_at_url.read()
+    image_at_url.close()
+    image = images.Image(image_bytes)
+    return image
+
+
+def get_thumbnail_image(new_image, url):
+    logging.info("get thumbnail image by url: {}".format(url))
+    image = get_image_by_url(url)
+    if new_image.image_height and new_image.image_width:
+        image = resize_image(image, new_image)
+    jpeg = images.images_service_pb.OutputSettings.JPEG
+    thumbnail = image.execute_transforms(output_encoding=jpeg, quality=new_image.quality)
+    return thumbnail
+
+
+def get_image_size(new_image):
+    image_at_url = urllib2.urlopen(str(new_image.image_url))
+    im = Image.open(image_at_url)
+    image_at_url.close()
+    new_image.image_width, new_image.image_height = im.size
+    return new_image
+
+
+def get_blob(new_image):
+    client = get_storage_client()
+    bucket = client.bucket(new_image.bucket_name)
+    blob = bucket.blob(new_image.image_name)
+    return blob
+
+
+def update_image_in_bd(db_image, new_image):
+    db_image.original_url = new_image.image_url
+    blob = get_blob(new_image)
+    db_image.processed_url = blob.public_url
+    db_image.save()
+
+
 class Cruncho_Image(db.Model):
-  name = db.StringProperty()
-  original_url = db.StringProperty()
-  processed_url = db.StringProperty()
-  created = db.DateTimeProperty(auto_now_add=True)
+    name = db.StringProperty()
+    original_url = db.StringProperty()
+    processed_url = db.StringProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
+class Filter(object):
+    def __init__(self, view, fields):
+        self.view = view
+        for name, key in fields.items():
+            if key == 'image_width' or key == 'image_height':
+                setattr(self, name, view.get(key))
+            else:
+                setattr(self, name, view.get(key, None))
+        setattr(self, 'bucket_name', settings.BUCKET_NAME)
+
+    @property
+    def create_task_save_image(self):
+        taskqueue.add(
+            url='/save_image',
+            params={'image_url': self.image_url,
+                    'image_name': self.image_name,
+                    'image_width': self.image_width,
+                    'image_height': self.image_height,
+                    'bucket_name': self.bucket_name}
+        )
 
 
 class MainPage(webapp2.RequestHandler):
-    image_name, image_url, bucket_name, image_width, image_height, quality = None, None, None, None, None, None
-
-    def resize_image(self, image):
-        image.resize(width=int(self.image_width), height=int(self.image_height))
-        return image
-
-    def get_thumbnail_image(self, url):
-        logging.info("get thumbnail image by url: {}".format(url))
-        image_at_url = urllib2.urlopen(url)
-        image_bytes = image_at_url.read()
-        image = images.Image(image_bytes)
-        if self.image_height and self.image_width:
-            image = self.resize_image(image)
-        jpeg = images_service_pb.OutputSettings.JPEG
-        thumbnail = image.execute_transforms(output_encoding=jpeg, quality=self.quality)
-        return thumbnail
-
-    def get_image_size(self):
-        image_at_url = urllib2.urlopen(str(self.image_url))
-        im = Image.open(image_at_url)
-        self.image_width, self.image_height = im.size
-
     def get(self):
         self.response.headers.add_header('Access-Control-Allow-Origin', settings.ACCESS_CONTROL_ALLOW_ORIGIN)
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        fields = {
+            'quality': 'quality',
+            'image_width': 'image_width',
+            'image_height': 'image_height',
+            'image_name': 'image_name',
+            'image_url': 'image_url',
+        }
+
+        new_image = Filter(self.request, fields)
+
         if self.request.headers.get('Host') not in settings.ACCESS_LIST:
             return self.response.write('\n\nYou do not have access rights!\n')
-        self.bucket_name = settings.BUCKET_NAME
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.quality = self.request.get('quality', None)
-        self.image_width = self.request.get('image_width')
-        self.image_height = self.request.get('image_height')
-        self.image_name = self.request.get('image_name')
-        self.image_url = self.request.get('image_url')
 
-        if self.image_url and self.image_name:
+        if new_image.image_url and new_image.image_name:
             self.response.headers['Content-Type'] = 'image/jpeg'
-            keyname = self.image_name
-            db_image = Cruncho_Image.get_or_insert(keyname, name=self.image_name)
+            db_image = Cruncho_Image.get_or_insert(new_image.image_name, name=new_image.image_name)
 
             if db_image.processed_url:
-                if self.image_height and self.image_width:
-                    return self.response.write(self.get_thumbnail_image(str(db_image.processed_url)))
+                logging.info("bucket_name: {}".format(new_image.bucket_name))
+                logging.info("db_image.processed_url: {}".format(db_image.processed_url))
+                if new_image.image_height and new_image.image_width:
+                    return self.response.write(get_thumbnail_image(new_image, db_image.processed_url))
                 return webapp2.redirect(str(db_image.processed_url))
-            taskqueue.add(
-                url='/save_image',
-                params={'image_url': self.image_url,
-                        'image_name': self.image_name,
-                        'image_width': self.image_width,
-                        'image_height': self.image_height,
-                        'bucket_name': self.bucket_name})
 
-            db_image.original_url = self.image_url
-            client = get_storage_client()
-            bucket = client.bucket(self.bucket_name)
-            blob = bucket.blob(self.image_name)
-            db_image.processed_url = blob.public_url
-            db_image.save()
+            new_image.create_task_save_image
+            update_image_in_bd(db_image, new_image)
 
-            if not self.image_height and not self.image_width:
-                self.get_image_size()
-            return self.response.write(self.get_thumbnail_image(str(db_image.original_url)))
+            if not new_image.image_height and not new_image.image_width:
+                new_image = get_image_size(new_image)
+            return self.response.write(get_thumbnail_image(new_image, db_image.original_url))
         return self.response.write('\n\nNo file name or URL!\n')
 
 
 class SaveImageOnStorage(webapp2.RequestHandler):
-    image_name, image_url, bucket_name, image_width, image_height = None, None, None, None, None
-
-    def resize_image(self, image):
-        image.resize(width=int(self.image_width), height=int(self.image_height))
-        return image
-
     def post(self):
-        self.image_name = self.request.get('image_name')
-        self.image_url = self.request.get('image_url')
-        self.image_width = self.request.get('image_width')
-        self.image_height = self.request.get('image_height')
-        self.bucket_name = self.request.get('bucket_name')
+        fields = {
+            'quality': 'quality',
+            'image_width': 'image_width',
+            'image_height': 'image_height',
+            'image_name': 'image_name',
+            'image_url': 'image_url',
+        }
 
-        client = get_storage_client()
-        bucket = client.bucket(self.bucket_name)
-        blob = bucket.blob(self.image_name)
+        new_image = Filter(self.request, fields)
 
-        image_at_url = urllib2.urlopen(self.image_url)
-        image_bytes = image_at_url.read()
-        image_at_url.close()
-
-        image = images.Image(image_bytes)
-        if self.image_width and self.image_height:
-            image = self.resize_image(image)
+        blob = get_blob(new_image)
+        image = get_image_by_url(new_image.image_url)
+        logging.info("self.image_width: {}".format(new_image.image_width))
+        logging.info("self.image_height: {}".format(new_image.image_height))
+        if new_image.image_width and new_image.image_height:
+            image = resize_image(image, new_image)
         image.im_feeling_lucky()
-        jpeg = images_service_pb.OutputSettings.JPEG
-        thumbnail = image.execute_transforms(output_encoding=jpeg, quality=80)
+        jpeg = images.images_service_pb.OutputSettings.JPEG
+        thumbnail = image.execute_transforms(output_encoding=jpeg, quality=new_image.quality)
         blob.upload_from_string(thumbnail, content_type='image/jpeg')
 
-        logging.info("image public url: {}".format(blob.public_url))
+
+class UploadImageOnStorage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers.add_header('Access-Control-Allow-Origin', settings.ACCESS_CONTROL_ALLOW_ORIGIN)
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        if self.request.headers.get('Host') not in settings.ACCESS_LIST:
+            return self.response.write('\n\nYou do not have access rights!\n')
+
+        fields = {
+            'quality': 'quality',
+            'image_width': 'image_width',
+            'image_height': 'image_height',
+            'image_name': 'image_name',
+            'image_url': 'image_url',
+        }
+
+        new_image = Filter(self.request, fields)
+
+        if self.request.headers.get('Host') not in settings.ACCESS_LIST:
+            return self.response.write('\n\nYou do not have access rights!\n')
+
+        if new_image.image_url and new_image.image_name:
+            self.response.headers['Content-Type'] = 'image/jpeg'
+            db_image = Cruncho_Image.get_or_insert(new_image.image_name, name=new_image.image_name)
+            blob = get_blob(new_image)
+            update_image_in_bd(db_image, new_image)
+            new_image.create_task_save_image
+
+            return self.response.write(blob.public_url)
+        return self.response.write('\n\nNo file name or URL!\n')
 
 
 # class AdminPage(webapp2.RequestHandler):
@@ -159,10 +220,15 @@ class SaveImageOnStorage(webapp2.RequestHandler):
 #         self.response.write(
 #             '<html><body>{}</body></html>'.format(greeting))
 
-app = webapp2.WSGIApplication([('/img', MainPage),
-                               # ('/admin', AdminPage),
-                               # ('/auth', AuthPage),
-                               ('/save_image', SaveImageOnStorage)], debug=True)
+
+app = webapp2.WSGIApplication([
+    ('/img', MainPage),
+    ('/upload_image', UploadImageOnStorage)
+    ('/save_image', SaveImageOnStorage)
+    # ('/admin', AdminPage),
+    # ('/auth', AuthPage),
+], debug=True)
+
 
 
 # webapp = webapp2.WSGIApplication([
@@ -172,6 +238,6 @@ app = webapp2.WSGIApplication([('/img', MainPage),
 #     ('/save_image', SaveImageOnStorage)
 # ], debug=True)
 #
-# app = CorsApplication(webapp, CorsOptions(allow_origins=['crunchoo.com'],
+# app = CorsApplication(webapp, CorsOptions(allow_origins=['cruncho.com'],
 #                                           allow_headers=['X-Foo'],
 #                                           continue_on_error=True))
