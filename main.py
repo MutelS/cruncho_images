b19e284a17a610b581ce3977bcff1e98301ddf2a
sergei <mutel.sergey@gmail.com>
2018-04-12 12:41:06 +0300

    pywt

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import logging
 import urllib2
 import webapp2
 import settings
+import numpy
+from pywt import dwt2
 
 from PIL import Image
-
 from google.appengine.api import images, taskqueue
 from google.cloud.storage import Client
 from google.appengine.ext import db
@@ -46,7 +47,7 @@ def get_thumbnail_image(new_image, url):
 
 def get_image_size(new_image):
     image_at_url = urllib2.urlopen(str(new_image.image_url))
-    im = Image.open(image_at_url)
+    im = PIL.Image.open(image_at_url)
     image_at_url.close()
     new_image.image_width, new_image.image_height = im.size
     return new_image
@@ -66,10 +67,11 @@ def update_image_in_bd(db_image, new_image):
     db_image.save()
 
 
-class Cruncho_Image(db.Model):
+class CrunchoImage(db.Model):
     name = db.StringProperty()
     original_url = db.StringProperty()
     processed_url = db.StringProperty()
+    blurred = db.StringProperty()
     created = db.DateTimeProperty(auto_now_add=True)
 
 
@@ -96,6 +98,7 @@ class Filter(object):
 
 
 class MainPage(webapp2.RequestHandler):
+
     def get(self):
         self.response.headers.add_header('Access-Control-Allow-Origin', settings.ACCESS_CONTROL_ALLOW_ORIGIN)
         self.response.headers['Content-Type'] = 'text/plain'
@@ -106,6 +109,7 @@ class MainPage(webapp2.RequestHandler):
             'image_height': 'image_height',
             'image_name': 'image_name',
             'image_url': 'image_url',
+            'blurred': 'blurred'
         }
 
         new_image = Filter(self.request, fields)
@@ -115,7 +119,14 @@ class MainPage(webapp2.RequestHandler):
 
         if new_image.image_url and new_image.image_name:
             self.response.headers['Content-Type'] = 'image/jpeg'
-            db_image = Cruncho_Image.get_or_insert(new_image.image_name, name=new_image.image_name)
+
+            db_image = CrunchoImage.get_or_insert(new_image.image_name, name=new_image.image_name)
+
+            if not db_image.blurred:
+                new_image.blurred = check_image_for_blurring(new_image)
+
+            if db_image.blurred == 'blurred':
+                return self.response.write(get_thumbnail_image(new_image, db_image.original_url))
 
             if db_image.processed_url:
                 logging.info("bucket_name: {}".format(new_image.bucket_name))
@@ -180,7 +191,7 @@ class UploadImageOnStorage(webapp2.RequestHandler):
 
         if new_image.image_url and new_image.image_name:
             self.response.headers['Content-Type'] = 'image/jpeg'
-            db_image = Cruncho_Image.get_or_insert(new_image.image_name, name=new_image.image_name)
+            db_image = CrunchoImage.get_or_insert(new_image.image_name, name=new_image.image_name)
             blob = get_blob(new_image)
             update_image_in_bd(db_image, new_image)
             new_image.create_task_save_image
@@ -221,15 +232,102 @@ class UploadImageOnStorage(webapp2.RequestHandler):
 #             '<html><body>{}</body></html>'.format(greeting))
 
 
+def check_image_for_blurring(new_image):
+    thresh = 35
+    MinZero = 0.03
+
+    image_at_url = urllib2.urlopen(str(new_image.image_url))
+    im = Image.open(image_at_url).convert('F')
+    x = numpy.asarray(im)
+    x_cropped = x[0:(numpy.shape(x)[0] / 16) * 16 - 1, 0:(numpy.shape(x)[1] / 16) * 16 - 1]
+    LL1, (LH1, HL1, HH1) = dwt2(x_cropped, 'haar')
+    LL2, (LH2, HL2, HH2) = dwt2(LL1, 'haar')
+    LL3, (LH3, HL3, HH3) = dwt2(LL2, 'haar')
+    Emap1 = numpy.square(LH1) + numpy.square(HL1) + numpy.square(HH1)
+    Emap2 = numpy.square(LH2) + numpy.square(HL2) + numpy.square(HH2)
+    Emap3 = numpy.square(LH3) + numpy.square(HL3) + numpy.square(HH3)
+
+    dimx = numpy.shape(Emap1)[0] / 8
+    dimy = numpy.shape(Emap1)[1] / 8
+    Emax1 = []
+    vert = 1
+    for j in range(0, dimx - 2):
+        horz = 1;
+        Emax1.append([])
+        for k in range(0, dimy - 2):
+            Emax1[j].append(numpy.max(numpy.max(Emap1[vert:vert + 7, horz:horz + 7])))
+            horz = horz + 8
+        vert = vert + 8
+
+    dimx = numpy.shape(Emap2)[0] / 4
+    dimy = numpy.shape(Emap2)[1] / 4
+    Emax2 = []
+    vert = 1
+    for j in range(0, dimx - 2):
+        horz = 1;
+        Emax2.append([])
+        for k in range(0, dimy - 2):
+            Emax2[j].append(numpy.max(numpy.max(Emap2[vert:vert + 3, horz:horz + 3])))
+            horz = horz + 4
+        vert = vert + 4
+
+    dimx = numpy.shape(Emap3)[0] / 2
+    dimy = numpy.shape(Emap3)[1] / 2
+    Emax3 = []
+    vert = 1
+    for j in range(0, dimx - 2):
+        horz = 1;
+        Emax3.append([])
+        for k in range(0, dimy - 2):
+            Emax3[j].append(numpy.max(numpy.max(Emap3[vert:vert + 1, horz:horz + 1])))
+            horz = horz + 2
+        vert = vert + 2
+
+    N_edge = 0
+    N_da = 0
+    N_rg = 0
+    N_brg = 0
+
+    EdgeMap = []
+    for j in range(0, dimx - 2):
+        EdgeMap.append([])
+        for k in range(0, dimy - 2):
+            if (Emax1[j][k] > thresh) or (Emax2[j][k] > thresh) or (Emax3[j][k] > thresh):
+                EdgeMap[j].append(1)
+                N_edge = N_edge + 1
+                rg = 0
+                if (Emax1[j][k] > Emax2[j][k]) and (Emax2[j][k] > Emax3[j][k]):
+                    N_da = N_da + 1
+                elif (Emax1[j][k] < Emax2[j][k]) and (Emax2[j][k] < Emax3[j][k]):
+                    rg = 1
+                    N_rg = N_rg + 1
+                elif (Emax2[j][k] > Emax1[j][k]) and (Emax2[j][k] > Emax3[j][k]):
+                    rg = 1
+                    N_rg = N_rg + 1
+                if rg and (Emax1[j][k] < thresh):
+                    N_brg = N_brg + 1
+            else:
+                EdgeMap[j].append(0)
+
+    per = float(N_da) / N_edge
+    BlurExtent = float(N_brg) / N_rg
+    logging.info("per: {}".format(per))
+    if per > MinZero:
+        blurred = "not blurred"
+        logging.info("Not blurred")
+    else:
+        blurred = "blurred"
+        logging.info("Blurred")
+    logging.info("BlurExtent: {}".format(str(BlurExtent)))
+    return blurred
+
 app = webapp2.WSGIApplication([
     ('/img', MainPage),
-    ('/upload_image', UploadImageOnStorage)
+    ('/upload_image', UploadImageOnStorage),
     ('/save_image', SaveImageOnStorage)
     # ('/admin', AdminPage),
     # ('/auth', AuthPage),
 ], debug=True)
-
-
 
 # webapp = webapp2.WSGIApplication([
 #     ('/img', MainPage),
